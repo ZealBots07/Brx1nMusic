8e0122a12dfebc15a1a270ce007f76fc0b9dd750
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-08-09 22:22:36 +0530

    Add files via upload

--- AnonXMusic/utils/stream/stream.py
+++ AnonXMusic/utils/stream/stream.py
@@ -11,7 +11,7 @@ from AnonXMusic.misc import db
 from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
 from AnonXMusic.utils.exceptions import AssistantErr
 from AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AnonXMusic.utils.pastebin import Anonybin
+from AnonXMusic.utils.pastebin import AnonyBin
 from AnonXMusic.utils.stream.queue import put_queue, put_queue_index
 from AnonXMusic.utils.thumbnails import get_thumb
 
@@ -117,7 +117,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await Anonybin(msg)
+            link = await AnonyBin(msg)
             lines = msg.count("\n")
             car = os.linesep.join(msg.split(os.linesep)[:17]) if lines >= 17 else msg
             carbon = await Carbon.generate(car, randint(100, 10000000))
