31e10414ca26dd34908d7bff46596a19af36ed81
ZealBots07 <zealanime@protonmail.com>
2024-08-10 12:18:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://telegram.me/byte_bkc",
+    "https://github.com/ZealBors07/Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
