cc7096f94346e50bd088a048dbf79996a5dbadbb
ZealBots07 <zealanime@protonmail.com>
2024-08-10 11:23:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -109,13 +109,13 @@ DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 
 if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://zealanime", SUPPORT_CHANNEL):
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
         )
 
 if SUPPORT_CHAT:
-    if not re.match("(?:http|https)://zealanime_fun", SUPPORT_CHAT):
+    if not re.match("(?:http|https)://", SUPPORT_CHAT):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
         )
